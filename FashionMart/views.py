e1abaa409ac69e30fb5570a8ddd7a7e9921dbf8e
Mogii22 <mohithrajuu@gmail.com>
2021-10-11 02:36:31 +0530

    this is my first commit

---
+++ FashionMart/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render, get_object_or_404
+from store.models import Product
+
+def home(request):
+
+    products = Product.objects.all().filter(is_available=True)
+
+    context = {
+        'products' : products
+    }
+    return render(request, 'home.html', context)
