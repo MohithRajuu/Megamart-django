2f9ead12c3017ba998c15ce9d8960fa1f29d0849
MohithRajuu <mohithrajuu@gmail.com>
2021-10-13 06:38:29 +0530

    User Account Authentication

--- FashionMart/settings.py
+++ FashionMart/settings.py
@@ -133,3 +133,16 @@ STATICFILES_DIRS = [
 #media files configuration
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR/'media'
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
+
+
+#CONFIGURE EMAIL HOST
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587 #for gmail
+EMAIL_HOST_USER = 'mohicool30@gmail.com'
+EMAIL_HOST_PASSWORD = 'gurgoanmaha56'
+EMAIL_USE_TLS = True
